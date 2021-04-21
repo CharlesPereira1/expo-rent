090f2955bc2c76f7b35a7ffa11b4cd5c92632902
charlespereira1 <charlesti.ptu@gmail.com>
2021-04-20 23:19:51 -0300

    configure fonts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
 
 export default function App() {
+  let [fontLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold,
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Expo-Rent</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
