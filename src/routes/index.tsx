adc3aa9de0adb8a163d325d2a0439e4238681447
charlespereira1 <charlesti.ptu@gmail.com>
2021-04-22 21:26:13 -0300

    configure routes personalized

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Feather } from "@expo/vector-icons";
@@ -12,7 +13,19 @@ const routes: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen
+          options={{
+            title: "ALUGUE",
+            headerTitleStyle: { fontFamily: "Montserrat_700Bold" },
+            headerRight: () => (
+              <TouchableOpacity style={{ marginRight: 15 }}>
+                <Feather name="shopping-bag" size={24} color="#000" />
+              </TouchableOpacity>
+            ),
+          }}
+          name="home"
+          component={Home}
+        />
         <Stack.Screen name="detail" component={Detail} />
       </Stack.Navigator>
     </NavigationContainer>
