cee962e06843debd85ba2832738b1e2de0dc7b2a
Charles Pereira <charlesti.ptu@gmail.com>
2021-05-18 23:41:48 -0300

    create detail and styles

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { View } from "react-native";
+import { Feather, Ionicons } from "@expo/vector-icons";
 
-// import { Container } from './styles';
+import { styles } from "./styles";
 
 const Detail: React.FC = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <View style={styles.swiperContent}></View>
+    </View>
+  );
 };
 
 export default Detail;
--- src/pages/Detail/styles.ts
+++ src/pages/Detail/styles.ts
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: { flex: 1, backgroundColor: "#ffF" },
+  swiperContentL: { flexDirection: "row", height: 340, width: "100%" },
 });
