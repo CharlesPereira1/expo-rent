db7e3b3fa99a67b8d2b48862a66de11865efe695
Charles Pereira <charlesti.ptu@gmail.com>
2021-05-23 23:57:36 -0300

    page detail with stars and icons

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Feather, Ionicons } from "@expo/vector-icons";
 import { ScrollView } from "react-native-gesture-handler";
 import Stars from "react-native-stars";
 
@@ -21,7 +22,29 @@ const Detail: React.FC = () => {
         <View style={{ width: "35%" }}>
           <Text style={styles.rating}>Avaliações</Text>
           <View style={{ alignItems: "center", flexDirection: "row" }}>
-            <Stars />
+            <Stars
+              default={4}
+              count={5}
+              half={true}
+              starSize={20}
+              fullStar={
+                <Ionicons name="md-star" size={24} style={styles.myStarStyle} />
+              }
+              emptyStar={
+                <Ionicons
+                  name="md-star-outline"
+                  size={24}
+                  style={styles.myStarStyle}
+                />
+              }
+              halfStar={
+                <Ionicons
+                  name="md-star-outline-half"
+                  size={24}
+                  style={styles.myStarStyle}
+                />
+              }
+            />
           </View>
         </View>
       </View>
